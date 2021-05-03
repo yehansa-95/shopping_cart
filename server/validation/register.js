67af4b1c3149da15cb2bcf0017902caf113eb686
pasan <pasaninduwara.emedia@gmail.com>
2021-05-03 08:38:03 +0530

    setup further for development

--- server/validation/register.js
+++ server/validation/register.js
@@ -1,35 +1,35 @@
 const Validator = require("validator");
-const isEmpty = require("is-empty");
+const empty = require("is-empty");
 
 module.exports = function validateRegisterInput(data) {
-    let errors = [];
+    let errors = {};
+    
+    data.name = !empty(data.name) ? data.name : "";
+    data.email = !empty(data.email) ? data.email : "";
+    data.password = !empty(data.password) ? data.password : "";
+    data.cpassword = !empty(data.password2) ? data.cpassword : "";
 
-    data.name = !isEmpty(data.name) ? data.name : "";
-    data.email = !isEmpty(data.email) ? data.email : "";
-    data.password = !isEmpty(data.password) ? data.password : "";
-    data.password2 = !isEmpty(data.password2) ? data.password2 : "";
-
-    if (Validator.isEmpty(data.name)) {
-        errors.push("Name field is required")
+    if (Validator.empty(data.name)) {
+        errors.name = "Name field is required";
     }
 
-    if (Validator.isEmpty(data.email)) {
+    if (Validator.empty(data.email)) {
         errors.email = "Email field is required";
-    } else if (!Validator.isEmail(data.email)) {
-        errors.push("Email is invalid");
+    } else if (!Validator.empty(data.email)) {
+        errors.email = "Email is invalid";
     }
 
-    if (Validator.isEmpty(data.password)) {
-        errors.push("Password field is required");
+    if (Validator.empty(data.password)) {
+        errors.password = "Password field is required";
     }
-    if (Validator.isEmpty(data.password2)) {
-        errors.push("Confirm password field is required");
+    if (Validator.empty(data.cpassword)) {
+        errors.cpassword = "Confirm password field is required";
     }
     if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-        errors.push("Password must be at least 6 characters");
+        errors.password = "Password must be at least 6 characters";
     }
-    if (!Validator.equals(data.password, data.password2)) {
-        errors.push("Passwords must match");
+    if (!Validator.equals(data.password, data.cpassword)) {
+        errors.cpassword = "Passwords must match";
     }
     return {
         errors,
